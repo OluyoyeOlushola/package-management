5f86e43c1814d3b4bd22422e696e8c6e1a7f413a
OluyoyeOlushola <126034838+OluyoyeOlushola@users.noreply.github.com>
2023-03-16 15:44:18 -0600

    Update install-tomcat.sh

--- Tomcat-installation/install-tomcat.sh
+++ Tomcat-installation/install-tomcat.sh
@@ -27,10 +27,20 @@ echo "end on tomcat installation"
 #Tomcat server configuration:
 find / -name server.xml context.xml
 vim /opt/tomcat9/conf/server.xml
-vi /opt/tomcat9/webapps/manager/META-INF/context.xml
-vi /opt/tomcat9/conf/tomcat-user.xml  # to add user
 
-	<user username="landmark" password="admin" roles="manager-gui,admin-gui"/>
+vi /opt/tomcat9/webapps/manager/META-INF/context.xml
+#add comment (<!--  -->) just as below
+ <!--
+  <Valve className="org.apache.catalina.valves.RemoteAddrValve"
+          allow="127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1" />
+          -->
+
+vi /opt/tomcat9/conf/tomcat-users.xml  # to add user
+# edit usernames, password and roles below and paste just before the last closing tag 
+<user username="role1" password="<must-be-changed>" roles="role1"/>
+
+
+	<user username="landmark" password="admin" roles="manager-gui,admin-gui,manager-script"/>
 	
 
 /opt/tomcat9/conf/context.xml
